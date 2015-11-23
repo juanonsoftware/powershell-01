1b60dd8403184e0910067d02c407cefeb66830b2
juanonsoftware <juanonsoftware@github.com>
2015-11-23 14:02:45 +0700

    Simple DLL to test LoadAssembly in PowerShell

---
+++ Samples/SimleAssembly/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace SimleAssembly
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Print()
+        {
+            return string.Format("FullName {0}, Age {1}", FullName, Age);
+        }
+    }
+}
